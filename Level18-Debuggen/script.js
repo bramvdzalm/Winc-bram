61c693c524ce6603c1adb46b6cb529b5b97568c8
Bram van der Zalm <bramvdzalm@hotmail.com>
2021-02-17 09:47:10 +0100

    add new files

---
+++ Level18-Debuggen/script.js
@@ -0,0 +1,24 @@
+const array = [
+    { name: "N. Armstrong", profession: "spacecowboy", age: 89 },
+    { name: "H. de Haan", profession: "kippen hypnotiseur", age: 59 },
+    { name: "A. Curry", profession: "kikvorsman", age: 32 },
+    { name: "F. Vonk", profession: "slangenmelker", age: 36 },
+    { name: "B. Bunny", profession: "konijnen uitlaatservice", age: 27 },
+    { name: "Dr.Evil", profession: "digital overlord", age: 56 }
+  ];
+
+  
+  
+  for (let person of array) {
+//     console.log("everything about this person: ", person)
+//     console.log("All names of the persons are: ", person.name)
+// 
+//     console.log("The year of birth from", person.name, "is: ", (2021 - person.age))
+// 
+//     console.log(person.name, "is a", person.profession)
+// 
+//     if (person.age >= 50) {
+//       console.log("You are older then 50:", person.name)
+//     }
+
+  }
