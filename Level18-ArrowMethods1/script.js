5bbc962c3667a352a6e946b330c159e11b8994c4
Bram van der Zalm <bramvdzalm@hotmail.com>
2021-02-17 13:24:53 +0100

    add map Level18 arrow methods

---
+++ Level18-ArrowMethods1/script.js
@@ -0,0 +1,55 @@
+// const addTheWordCool = function(array) {
+//     array.push("cool");
+//     return array;
+//   };
+// 
+// console.log("Add cool:", addTheWordCool(["nice", "awesome", "tof"]));
+
+
+// const amountOfElementsInArray = function(array) {
+//     return array.length
+//   };
+// console.log(amountOfElementsInArray(['appels', 'peren', 'citroenen'])); 
+// 
+
+
+
+// const alastElementInArray = function(array) {
+//     return array[3]
+//   }
+// 
+// console.log(alastElementInArray(["Haas", "Cavia", "Kip", "Schildpad"])) 
+
+// const presidents = ["Trump", "Obama", "Bush", "Clinton"] 
+//   
+//   const impeachTrumpSlice = function(array) {
+//    let remove = array.slice(1, 4)
+//    console.log(remove)
+//    return remove 
+//   }
+//   const impeachTrumpSplice = function(array) {
+//     let remove2 = array.splice(0, 1)
+//     console.log(remove2)
+//     return remove2
+//   }
+//   
+//   console.log(impeachTrumpSlice(presidents)); // ["Obama", "Bush", "Clinton"]
+//   console.log(impeachTrumpSplice(presidents)); // ["Obama", "Bush", "Clinton"]
+
+// const stringsTogether = function(array) {
+//          return array.join("+")
+//       };
+// 
+// console.log(stringsTogether(['Winc', 'Academy', 'is', 'leuk', ';-}'])) 
+// 
+// const stringsTogether = function(array) {
+//          return array.join("+")
+//       };
+// 
+// console.log(stringsTogether(['Winc', 'Academy', 'is', 'leuk', ';-}']))
+// 
+// const combineArrays = function(array1, array2) {
+//     return array1.concat(array2)
+//  };
+// 
+// console.log(combineArrays([1, 2, 3], [4, 5, 6]))
