bb2be769bee6ea5b645c2ad8eda873ae030b4bc3
bramvdzalm <bramvdzalm@hotmail.com>
2022-01-15 09:32:31 +0100

    add debug galgje exercise

---
+++ debug-react-galgje-master/HangmanReact-exercise-5/src/components/GuessesLeft/GuessesLeft.js
@@ -0,0 +1,14 @@
+import React from "react";
+
+const GuessesLeft = props => (
+  
+  <p className="lives">
+    You have {props.maxGuesses - props.wrongLetters.length} guesses remaining
+    
+  </p>
+  
+);
+
+
+
+export default GuessesLeft;
